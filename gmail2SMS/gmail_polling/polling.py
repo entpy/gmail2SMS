a9daca86a495908a2284060544984db830a2b23b
developer <developer@entpy.com>
2017-01-10 17:29:22 +0100

    test

--- gmail2SMS/gmail_polling/polling.py
+++ gmail2SMS/gmail_polling/polling.py
@@ -37,8 +37,9 @@ class GmailPolling():
 
     def get_unread_email(self):
         """List of unread email"""
-        # prelevo l'elenco di email in base a determinati criteri
-        emails = self.gmail_imap.inbox().mail(on=date.today(), unread=True, sender=local_settings.sender)
+        # prelevo l'elenco di email in base a determinati criteri TODO: riabilitare il primo
+        # emails = self.gmail_imap.inbox().mail(on=date.today(), unread=True, sender=local_settings.sender)
+        emails = self.gmail_imap.inbox().mail(on=date.today(), unread=True)
         for email in emails:
             # prelevo i dati per ogni singola email (oggetto, testo, ...)
             email.fetch()
@@ -48,8 +49,8 @@ class GmailPolling():
             if email_subject.find("Allarme") > -1 or email_subject.find("Fin.All.") > -1:
                 # marco la mail come letta
                 email.read()
-                # invio l'sms
-                self.send_sms(text=email_subject)
+                # invio l'sms TODO: riabilitare
+                # self.send_sms(text=email_subject)
 
         # fix per far riscaricare i messaggi della inbox, la libreria cachava tutto e se
         # arrivava un nuovo messaggio non veniva tirato giù, ho solo resettato alcuni campi
